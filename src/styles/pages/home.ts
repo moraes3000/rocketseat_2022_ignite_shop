51d05476820057c8480c6c8379c3e3df769a8b21
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-09-08 12:29:44 -0300

    Criando carrossel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,9 +1,9 @@
-import { styled } from "../styles"
+import { styled } from ".."
 
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
+  // gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
