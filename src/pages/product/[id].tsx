b9fdf0ae64c503e7f5addb0217e20180a30cbb7e
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-09-08 18:41:07 -0300

    Estrutura da página de produto

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+
+
+export default function Product() {
+  return (
+    <>
+      <ProductContainer>
+        <ImageContainer>
+
+        </ImageContainer>
+        <ProductDetails>
+          <h1>CamisaX</h1>
+          <span>79</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo impedit tempore nostrum earum accusamus. Nulla distinctio maxime sit vero ipsa rem harum vitae dicta doloribus. Earum porro deleniti repudiandae fugiat.</p>
+
+          <button>Comprar</button>
+
+        </ProductDetails>
+      </ProductContainer>
+    </>
+  )
+}
