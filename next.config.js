ac37dae15331afe141a90b2b2751b126802e1481
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-09-09 12:45:23 -0300

    Estrutura de sucesso

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
 
   experimental: {
+    newNextLinkBehavior: true,
     images: {
       allowFutureImage: true,
     },
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <>
-      <h1>Sucesso</h1>
-    </>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>Uhuul <strong>Nome</strong> , sua <strong>camiseta</strong> já está a caminho da sua casa</p>
+
+      <Link href='/'>
+        voltar ao catalogo
+      </Link>
+    </SuccessContainer>
   )
 }
